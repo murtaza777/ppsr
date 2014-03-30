481971933277fb60389a9ddda1a81c649715ed2e
Murtaza Kheriwala <murtaza.kheriwala@gmail.com>
2014-03-31 00:24:40 +1100

    Removed the ppsr stubs from ws-cxf-client

--- ws-cxf-client/src/main/java/au/gov/ppsr/schemas/_2011/_04/services/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.ppsr.gov.au/2011/04/services", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package au.gov.ppsr.schemas._2011._04.services;
--- ws-cxf-client/src/main/java/au/gov/ppsr/schemas/faults/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.ppsr.gov.au/faults", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package au.gov.ppsr.schemas.faults;
