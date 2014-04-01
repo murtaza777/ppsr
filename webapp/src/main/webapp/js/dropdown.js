4dbafdb94be37ba6807903da9e80e02cecc5f037
Murtaza Kheriwala <murtaza.kheriwala@gmail.com>
2014-04-01 23:02:15 +1100

    Refactored dropdown.js, added sl4j dependencies in pom.xml and todo in ChangeB2GSpringClient to autowire the properties

--- webapp/src/main/webapp/js/dropdown.js
+++ webapp/src/main/webapp/js/dropdown.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
     //var fieldPosition = 0;
     $("#dropdown").change(function () {
-        //fieldPosition++;
-        var select = $("#dropdown").val();
-        if (select == 'blank') {
-            $("#requestForm").remove()
-        }
-        else {
-            $("#requestForm").remove()
+      //fieldPosition++;
+      var select = $("#dropdown").val();
+      $("#requestForm").remove()
+      if (select != 'blank') {
             $.get("./" + select,
                 function (data) {
                     $("#selection").after(data);
--- ws-cxf-client/src/main/java/au/gov/ppsr/client/ChangeB2GSpringClient.java
+++ ws-cxf-client/src/main/java/au/gov/ppsr/client/ChangeB2GSpringClient.java
@@ -1,22 +1,16 @@
 package au.gov.ppsr.client;
 
-import au.gov.ppsr.client.header.handler.ClientPasswordCallback;
+import au.gov.ppsr.client.header.HeaderAuthentication;
 import au.gov.ppsr.client.request.ChangeB2GPasswordRequest;
 import au.gov.ppsr.client.response.ChangePasswordResponse;
 import au.gov.ppsr.exception.PpsrException;
-import au.gov.ppsr.schemas._2011._04.services.*;
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.endpoint.Endpoint;
-import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
-import org.apache.ws.security.WSConstants;
-import org.apache.ws.security.handler.WSHandlerConstants;
+import au.gov.ppsr.schemas._2011._04.services.ChangeB2GPasswordResponseMessage;
+import au.gov.ppsr.schemas._2011._04.services.ObjectFactory;
+import au.gov.ppsr.schemas._2011._04.services.RegisterOperationsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * User: murtaza
@@ -24,6 +18,11 @@ import java.util.Map;
  * Time: 10:21 PM
  */
 public class ChangeB2GSpringClient {
+  //TODO Autowire the properties from cxf.xml
+  /*@Autowired
+  private ObjectFactory objectFactory;
+  @Autowired
+  RegisterOperationsService changePasswordClient;*/
 
   private static final Logger LOG = LoggerFactory.getLogger(ChangeB2GSpringClient.class);
 
@@ -35,39 +34,22 @@ public class ChangeB2GSpringClient {
     RegisterOperationsService port = (RegisterOperationsService) context.getBean("changePasswordClient");
     ObjectFactory factory = (ObjectFactory) context.getBean("objectFactory");
     ChangeB2GPasswordRequest request = new ChangeB2GPasswordRequest();
-    addAuthentication(port, username, password);
+    HeaderAuthentication.addAuthentication(port, username, password);
     request.request(factory, port, username, newPassword);
   }
 
   public ChangePasswordResponse invokeService(String username, String password, String newPassword) throws PpsrException {
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"cxf.xml"});
-    RegisterOperationsService port = (RegisterOperationsService) context.getBean("changePasswordClient");
-    ObjectFactory factory = (ObjectFactory) context.getBean("objectFactory");
+    RegisterOperationsService changePasswordClient = (RegisterOperationsService) context.getBean("changePasswordClient");
+    ObjectFactory objectFactory = (ObjectFactory) context.getBean("objectFactory");
     ChangeB2GPasswordRequest request = new ChangeB2GPasswordRequest();
-    addAuthentication(port, username, password);
-    ChangeB2GPasswordResponseMessage responseMessage = request.request(factory, port, username, newPassword);
+    HeaderAuthentication.addAuthentication(changePasswordClient, username, password);
+    ChangeB2GPasswordResponseMessage responseMessage = request.request(objectFactory, changePasswordClient, username, newPassword);
     return ChangePasswordResponse.build()
         .customersRequestMessageId(responseMessage.getChangeB2GPasswordResponse().getValue().getCustomersRequestMessageId())
         .ppsrTransactionId(responseMessage.getChangeB2GPasswordResponse().getValue().getPpsrTransactionId())
         .passwordExpiryDateTime(responseMessage.getChangeB2GPasswordResponse().getValue().getPasswordExpiryDateTime())
         .ppsrRequestMessageId(responseMessage.getChangeB2GPasswordResponse().getValue().getPpsrRequestMessageId())
         .requestProcessedDateTime(responseMessage.getChangeB2GPasswordResponse().getValue().getRequestProcessedDateTime());
-  }
-
-  private static void addAuthentication(RegisterOperationsService port, String username, String password) {
-    Client client = ClientProxy.getClient(port);
-    Endpoint endpoint = client.getEndpoint();
-    Map<String, Object> outProps = new HashMap<String, Object>();
-    outProps.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
-    outProps.put(WSHandlerConstants.USER, username);
-    outProps.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
-    ClientPasswordCallback handler = new ClientPasswordCallback();
-    handler.setUsername(username);
-    handler.setPassword(password);
-    outProps.put(WSHandlerConstants.PW_CALLBACK_REF, handler);
-    outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, ClientPasswordCallback.class.getName());
-    WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
-
-    endpoint.getOutInterceptors().add(wssOut);
   }
 }
